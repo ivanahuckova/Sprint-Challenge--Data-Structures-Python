b891aa5c37a15b04727709be2642e3873c6db40d
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-14 11:02:21 +0200

    Complete answers task

--- names/names.py
+++ names/names.py
@@ -1,9 +1,6 @@
 import time
-from bst import BinarySearchTree
 
 start_time = time.time()
-
-names_bst = BinarySearchTree("a")
 
 f = open('names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
@@ -14,21 +11,21 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 
-def comp(list1, list2):
-    duplicates = []
-    for val in list1:
-        if val in list2:
-            duplicates.append(val)
-    return duplicates
+# def comp(list1, list2):
+#     duplicates = []
+#     for val in list1:
+#         if val in list2:
+#             duplicates.append(val)
+#     return duplicates
 
 
-duplicates = comp(names_1, names_2)
+# duplicates = comp(names_1, names_2)
 
-# duplicates = []
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
+duplicates = []
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
