5485f9a416e507038613199a90ac47049b757778
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-14 10:17:27 +0200

    Data structure answers

---
+++ names/bst.py
@@ -0,0 +1,48 @@
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+
+            else:
+                self.left.insert(value)
+
+        if value >= self.value:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if target == self.value:
+            return True
+
+        elif self.left and target < self.value:
+            return self.left.contains(target)
+
+        elif self.right and target > self.value:
+            return self.right.contains(target)
+
+        else:
+            return False
+
+    def get_max(self):
+        current = self
+        while current:
+            if not current.right:
+                return current.value
+            current = current.right
+        return current.value
+
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
--- names/names.py
+++ names/names.py
@@ -1,12 +1,18 @@
 import time
+from bst import BinarySearchTree
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+
+def tree(self):
+    self.bst = BinarySearchTree("a")
+
+
+f = open('names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('names/names_1.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
@@ -17,6 +23,5 @@ for name_1 in names_1:
             duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
