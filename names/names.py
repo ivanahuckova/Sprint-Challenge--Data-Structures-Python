e4418a9b63ae68b9297c233c9aeb86399b966655
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-14 11:09:47 +0200

    Fix typo and get the result

--- names/names.py
+++ names/names.py
@@ -6,26 +6,26 @@ f = open('names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names/names_1.txt', 'r')
+f = open('names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 
-# def comp(list1, list2):
-#     duplicates = []
-#     for val in list1:
-#         if val in list2:
-#             duplicates.append(val)
-#     return duplicates
+def comp(list1, list2):
+    duplicates = []
+    for val in list1:
+        if val in list2:
+            duplicates.append(val)
+    return duplicates
 
 
-# duplicates = comp(names_1, names_2)
+duplicates = comp(names_1, names_2)
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
